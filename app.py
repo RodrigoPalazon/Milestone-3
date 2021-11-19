b94d0502cc682ec0e4ab27c2a4239e19de218237
Rodrigo Palazon <palazonbatera@gmail.com>
2021-11-19 16:15:32 +0100

    Set debug mode to False

--- app.py
+++ app.py
@@ -239,7 +239,7 @@ def delete_category(category_id):
 if __name__ == "__main__":
     app.run(host=os.environ.get("IP"),
             port=int(os.environ.get("PORT")),
-            debug=True)
+            debug=False)
 
 
 
